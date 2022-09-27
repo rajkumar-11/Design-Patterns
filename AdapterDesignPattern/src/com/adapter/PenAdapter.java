f5170f469152ad161b66e1c7a22a1abaf39fc24d
rajkumar-11 <rajkumarnavhaal@gmail.com>
2022-09-27 21:54:44 +0530

    adding apater design pattern

---
+++ AdapterDesignPattern/src/com/adapter/PenAdapter.java
@@ -0,0 +1,16 @@
+package com.adapter;
+
+import com.krishna.PilotPen;
+
+public class PenAdapter implements Pen
+{
+	PilotPen pp= new PilotPen();
+
+	@Override
+	public void write(String str) 
+	{
+			pp.mark(str);
+	}
+  
+	
+}
